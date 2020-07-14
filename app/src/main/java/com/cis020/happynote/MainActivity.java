afca2c22f1a48bac52b445c27dab8814d270d9e8
cis020 <insung020@gmail.com>
2020-07-14 21:54:09 +0900

    추가 : gradle 에 binding 옵션 추가

---
+++ app/src/main/java/com/cis020/happynote/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cis020.happynote;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
